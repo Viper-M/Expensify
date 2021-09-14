bfc101dbef8108b9eebb4b857baba0e8a092932e
Mohit Sharma <vipersharma.88@gmail.com>
2021-09-14 14:48:41 +0530

    init

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+var template = (
+    <div>
+        <h2>Hellow</h2>
+    </div>
+)
+ReactDOM.render(template, document.getElementById('root'))
